04ecc9c6fbda4b24d076455bd0afa478fa746665
Samuel Monção <samu.henr@gmail.com>
2021-07-09 18:00:00 -0300

    meta seo ajusts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,8 +5,14 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet" />
+          <meta name="description" content="Saiba as programações do Igreja Batista Lírio dos Vales IBLV. Saiba com ofertar e como entregar seu dízimo de maneira rápida e fácil." />
+          <meta name="og:title" property="og:title" content="Acessos IBLV | Uma Igreja em célula" />
+          <meta property="og:description" content="Saiba as programações do Igreja Batista Lírio dos Vales IBLV. Saiba com ofertar e como entregar seu dízimo de maneira rápida e fácil." />
+          <meta property="og:url" content="https://iblv-links.vercel.app/" />
+          <meta property="og:image" content="https://iblv-links.vercel.app/ogseo.png" />
+          <meta name="robots" content="index" />
         </Head>
         <body>
           <Main />
